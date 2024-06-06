797404acd83103c586d3da51ef8ef14277f1a5a0
Vikas Rathod <118325845+vikasrathod2002@users.noreply.github.com>
2024-06-06 22:29:34 +0530

    Create Bazoka and Mocha's Array.cpp

---
+++ Bazoka and Mocha's Array.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[222222];
+int main(){
+	int _;
+	cin>>_;
+	while(_--){
+		int n,f{0};
+		cin>>n;
+		for(int i=1; i<=n&&cin>>a[i]; i++) {
+			if(a[i]<a[i-1]) f++;
+		}
+		if(a[n]>a[1]) f++;
+		if(f>1) cout<<"NO\n";
+		else cout<<"YES\n";
+	}
+}
