4a7fe4c29dadc350069ebb41bb3a318929e47ec7
Vikas Rathod <118325845+vikasrathod2002@users.noreply.github.com>
2024-06-10 22:41:43 +0530

    Create Coin Games.cpp

---
+++ Coin Games.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int n;string s;cin>>n>>s;
+        cout<<(count(s.begin(),s.end(),'U')%2?"YES":"NO")<<"\n";
+ 
+    }
+}
